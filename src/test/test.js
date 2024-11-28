b2c08cc1c2279b184910acc24840af04cb6012f4
michalk1924 <michalk1924@gmail.com>
2024-11-28 14:38:55 +0200

    change somthing

--- src/test/test.js
+++ src/test/test.js
@@ -8,21 +8,17 @@ async function testDatabaseFunctions() {
   let db;
 
   try {
-    // התחברות לבסיס נתונים
     client = await connectDatabase();
     db = client.db(process.env.DB_NAME);
 
-    // בדיקה של insertDocument
     const testDoc = { name: "Test Doc", value: 10 };
     const insertResult = await insertDocument(client, "testCollection", testDoc);
     console.log("insertDocument result:", insertResult);
 
-    // בדיקה של getDocuments
     const { insertedId } = insertResult;
     const documents = await getDocuments(client, "testCollection", { _id: new ObjectId(insertedId) });
     console.log("getDocuments result:", documents);
 
-    // בדיקה של updateDocument
     const updatedDoc = { value: 15 };
     const updateResult = await updateDocument(client, "testCollection", insertedId.toString(), updatedDoc);
     console.log("updateDocument result:", updateResult);
@@ -30,7 +26,6 @@ async function testDatabaseFunctions() {
     const updatedDocuments = await getDocuments(client, "testCollection", { _id: new ObjectId(insertedId) });
     console.log("Updated document:", updatedDocuments);
 
-    // בדיקה של deleteDocument
     const deleteResult = await deleteDocument(client, "testCollection", insertedId.toString());
     console.log("deleteDocument result:", deleteResult);
 
@@ -40,12 +35,10 @@ async function testDatabaseFunctions() {
   } catch (error) {
     console.error("Error occurred:", error);
   } finally {
-    // אם client לא null, סגור את החיבור
     if (client) {
       await client.close();
     }
   }
 }
 
-// קריאה לפונקציה
 testDatabaseFunctions();
