463975223c2f9bfdad28575c5354ac1d29365f60
RacheliVa <racheli.litman@gmail.com>
2024-12-01 12:58:10 +0200

    colors

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,12 @@
 import styles from './home.module.css';
-import BudgetWise from '../BudgetWise.png';
-import Image from 'next/image';
+
 
 export default function Home() {
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 {/* שימוש ב-<Image> של Next.js */}
-                <Image src={BudgetWise} alt="BudgetWise Logo" width={150} height={50} />
+                {/* <Image src={BudgetWise} alt="BudgetWise Logo" width={150} height={50} /> */}
                 <nav className={styles.nav}>
                     <a href="#">Home</a>
                     <a href="#">Categories</a>
@@ -18,15 +17,15 @@ export default function Home() {
             </header>
             <main className={styles.main}>
                 <section className={styles.stats}>
-                    <div className={`${styles.statBox} ${styles.statBoxWithImage}`} style={{ backgroundColor: '#47cfcf' }}>
+                    <div className={`${styles.statBox} ${styles.statBoxWithImage7}`}>
                         <h2>CHA-CHING! MY MONTHLY INCOMES</h2>
                         <p>15,990</p>
                     </div>
-                    <div className={styles.statBox} style={{ backgroundColor: '#ff6b6b' }}>
+                    <div className={`${styles.statBox} ${styles.statBoxWithImage8}`}>
                         <h2>MY MONTHLY EXPENSES</h2>
                         <p>15,990</p>
                     </div>
-                    <div className={styles.statBox} style={{ backgroundColor: '#f3c969' }}>
+                    <div className={`${styles.statBox} ${styles.statBoxWithImage9}`}>
                         <h2>YOU SAVED THIS MONTH</h2>
                         <p>15,990</p>
                     </div>
@@ -36,10 +35,18 @@ export default function Home() {
                     <p>What would you like to do today?</p>
                 </section>
                 <section className={styles.actions}>
-                    <button>My Reports</button>
-                    <button>My Savings State</button>
-                    <button>Recommendations</button>
-                    <button>My Transactions</button>
+                    <button>My Reports
+                        <img src='/10.jpg'></img>
+                    </button>
+                    <button>My Savings State
+                    <img src='/11.jpg'></img>
+                    </button>
+                    <button>Recommendations
+                    <img src='/12.jpg'></img>
+                    </button>
+                    <button>My Transactions
+                    <img src='/13.jpg'></img>
+                    </button>
                 </section>
             </main>
         </div>
