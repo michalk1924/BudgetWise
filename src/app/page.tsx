1347ec2813b9193901c45546ff58dd7504ad7345
Yael Koppel <yaelkoppel1@gmail.com>
2024-12-02 09:53:37 +0200

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import ComingSoon from "./components/comingSoon/ComingSoon";
-import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
     <div>
-     <Header/>
 
       <ComingSoon />
     </div>
