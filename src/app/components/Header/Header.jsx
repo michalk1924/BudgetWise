51999078b70a964c32e8904ec46b68fd158df47d
Yael Koppel <yaelkoppel1@gmail.com>
2024-12-03 10:19:27 +0200

    commit

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 import styles from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Image src='/logo.png' alt="Logo" width={200} height={75} />
+      <Image src='/logo.png' alt="Logo" width={190} height={55} />
       <nav className={styles.navbar}>
         <div className={styles.brand}></div>
         <ul className={styles.menu}>
@@ -19,6 +21,10 @@ const Header = () => {
             <Image src='/brush_stain.png' alt="brush_stain" width={20} height={20} />
           </li>
           <li>
+            <Link href="/transactions">Transactions</Link>
+            <Image src='/brush_stain.png' alt="brush_stain" width={20} height={20} />
+          </li>
+          <li>
             <Link href="/savings">Savings</Link>
             <Image src='/brush_stain.png' alt="brush_stain" width={20} height={20} />
           </li>
@@ -28,6 +34,12 @@ const Header = () => {
           </li>
           <li>
             <Link href="/about-us">About Us</Link>
+            <Image src='/brush_stain.png' alt="brush_stain" width={20} height={20} />
+          </li>
+          <li className={styles.userAccount}>
+            <Link href="/account" title="Your Account">
+              <FontAwesomeIcon icon={faUser} />
+            </Link>
           </li>
         </ul>
       </nav>
