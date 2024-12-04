3ef0b31741ab8d33c78198aa6ad6c899ff7cae46
michalk1924 <michalk1924@gmail.com>
2024-12-04 10:02:17 +0200

    solve errors to build

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,3 +2,4 @@ export { default as ComingSoon } from './ComingSoon/ComingSoon';
 export { default as BudgetGrid } from './categoriesGrid/BudgetGrid/BudgetGrid';
 export { default as Header } from './Header/Header';
 export { default as TransactionTable } from './transactions/transactionsTable/TransactinsTable';
+export { default as SavingsGrid } from './SavingsGrid/SavingsGrid';
