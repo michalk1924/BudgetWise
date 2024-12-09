802c192eb440f79fbc3f2180971a49f4e7b2215e
Yael Koppel <yaelkoppel1@gmail.com>
2024-12-09 12:20:32 +0200

    commit

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -8,7 +8,7 @@ import GridItem from "../components/categoriesGrid/GridItem/GridItem";
 import { UserCategory } from "../../types/types";
 import useUserStore from "@/store/userStore";
 import { useState } from "react";
-import { useQueryClient, useMutation } from "react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import userService from "@/services/user";
 
 
