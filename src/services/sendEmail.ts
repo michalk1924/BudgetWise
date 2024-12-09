82cf94bbd5214c44d1ea2ca3e5457dfc874c0566
michalk1924 <michalk1924@gmail.com>
2024-12-09 10:49:30 +0200

    send email service

---
+++ src/services/sendEmail.ts
@@ -0,0 +1,46 @@
+"use server"
+
+const nodemailer = require('nodemailer');
+
+async function sendEmail(to: string, subject: string, text: string) {
+    if (!to || !subject || !text) {
+        throw new Error('Missing required parameters');
+    }
+    if (!validateEmail(to)) {
+        throw new Error('Invalid email format');
+    }    
+    
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_ADDRESS,
+            pass: process.env.EMAIL_PASSWORD
+        },
+        tls: {
+            rejectUnauthorized: false // השבתת אימות SSL
+            //לעבוד על זה!!!!!!!! אם יעשה בעיות גם במייל השני!!!!!!!
+        }
+    });
+
+
+    let mailOptions = {
+        from: 'michal21924@gmail.com',
+        to: to,
+        subject: subject,
+        text: text
+    };
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+    } catch (error) {
+        console.error('Error sending email: ', error);
+    }
+}
+
+function validateEmail(to: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(to);
+}
+
+export default sendEmail;
