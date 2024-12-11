871088ea06ed0aff1deafd86dcf5771c3041c9e6
Yael Koppel <yaelkoppel1@gmail.com>
2024-12-11 11:45:13 +0200

    commit

--- src/app/footerPages/content/page.tsx
+++ src/app/footerPages/content/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styles from "../footerPages.module.css";
+
 const ContentPolicyPage = () => {
     return (
-      <div className="page">
+      <div className={styles.page}>
         <h1>Content Policy</h1>
         <p>
           Our app encourages users to contribute responsibly. Any inappropriate content is strictly prohibited and may result in account suspension.
