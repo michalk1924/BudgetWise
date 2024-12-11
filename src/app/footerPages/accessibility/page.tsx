77de44377afee16e5ad66902aa0ed1e76f750c76
Yael Koppel <yaelkoppel1@gmail.com>
2024-12-11 11:45:13 +0200

    commit

--- src/app/footerPages/accessibility/page.tsx
+++ src/app/footerPages/accessibility/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import styles from "../footerPages.module.css";
+
 const AccessibilityStatementPage = () => {
     return (
-      <div className="page">
+      <div className={styles.page}>
         <h1>Accessibility Statement</h1>
         <p>
           We strive to make our app accessible to everyone, including individuals with disabilities. If you face any issues, please contact us for assistance.
