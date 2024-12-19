f46b7117c78d4afe0ebc48c9dee6e4009b8bbd37
michalk1924 <michalk1924@gmail.com>
2024-12-19 11:45:33 +0200

    do api for stlouisfed

---
+++ src/app/api/stlouisfed/[category]/route.ts
@@ -0,0 +1,24 @@
+
+"use server"
+
+import { NextResponse, NextRequest } from "next/server";
+import axios from "axios";
+
+export async function GET(request: NextRequest, { params }: { params: any }) {
+    const { category } = await params;
+    try {
+        const apiKey = process.env.API_KEY;
+        
+        const url = `https://api.stlouisfed.org/fred/series/observations?series_id=${category}&api_key=${apiKey}&file_type=json`;
+        const response = await axios.get(url);
+        return NextResponse.json(response.data);
+        
+    }
+    catch (error: any) {
+        console.error(`Error fetching data for ${category}`, error.message);
+        return NextResponse.json({
+            message: `Error fetching data for ${category}`,
+            status: 500
+        });
+    }
+}
