d538fc54a3b727bbfab8b33465f2c4b65c0e717d
michalk1924 <michalk1924@gmail.com>
2024-12-25 11:32:28 +0200

    token time in layout

--- src/services/cookies.ts
+++ src/services/cookies.ts
@@ -1,7 +1,9 @@
 import Cookies from 'js-cookie';
 
 export const saveToken = (token: string) => {
+    const expiryTime = new Date().getTime() + 12 * 60 * 60 * 1000;
     Cookies.set('authToken', token, { expires: 12 / 24, path: '' });
+    Cookies.set('authTokenExpiry', expiryTime.toString(), { expires: 12 / 24, path: '' });
 };
 
 export const getToken = () => {
